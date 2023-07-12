893df8025c75fed4e9ca96ee5f6dd4acc9a64237
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-12 10:30:28 +0530

    [RK Nanda] Adding class to give Fibonacci term

---
+++ LogicalProblems6/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LogicalProblems6
+{
+	public class Fibonacci
+	{
+		public int Fib(int n)
+		{
+			
+			if(n<=1) {
+				return n;
+			}
+			else {
+					
+				return Fib(n - 1) + Fib(n - 2);
+			}
+		}
+	}
+}
+
--- LogicalProblems6/Program.cs
+++ LogicalProblems6/Program.cs
@@ -3,7 +3,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        bool flag = true;
+        int option;
         Console.WriteLine("Welcome to Logical Problem Assignment!");
+        while (flag)
+        {
+            Console.WriteLine("MENU:->");
+            Console.WriteLine("1. Fibonacci Series");
+            Console.WriteLine("0. Exit");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 0:
+                    {
+                        flag = false;
+                        break;
+                    }
+                case 1:
+                    {
+                        int n;
+                        Fibonacci f = new Fibonacci();
+                        Console.WriteLine("Enter Number to get Fibonacci Sequence");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"The {n}th Fibonacci term is {f.Fib(n-1)}");
+                        break;
+                    }
+
+
+
+            }
+        }
     }
 }
 
