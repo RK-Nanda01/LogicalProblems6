223453e38433c6955d5b5e84f8552b50a12288f3
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-12 11:02:49 +0530

    [RK Nanda] Adding class to check if number is Prime

---
+++ LogicalProblems6/PrimeNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LogicalProblems6
+{
+	public class PrimeNumber
+	{
+		public int IsPrime(int n)
+		{
+			if(n == 1 || n==0) {
+				return -1;
+			}
+
+			for(int i =2; i<=Math.Sqrt(n); i++)
+			{
+				if(n%i == 0)
+				{
+					return 0;
+				}
+			}
+			return 1;
+		}
+	}
+}
+
--- LogicalProblems6/Program.cs
+++ LogicalProblems6/Program.cs
@@ -11,6 +11,7 @@ class Program
             Console.WriteLine("MENU:->");
             Console.WriteLine("1. Fibonacci Series");
             Console.WriteLine("2. Check for Perfect Number");
+            Console.WriteLine("3. Check for Prime Number");
             Console.WriteLine("0. Exit");
             option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -45,6 +46,28 @@ class Program
                         else
                         {
                             Console.WriteLine($"The number {n} is Not Perfect");
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        int n;
+                        int res;
+                        PrimeNumber pr = new PrimeNumber();
+                        Console.WriteLine("Enter Number to Check for Prime Number");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        res = pr.IsPrime(n);
+                        if (res == 1)
+                        {
+                            Console.WriteLine($"The number {n} is Prime");
+                        }
+                        else if(res == 0)
+                        {
+                            Console.WriteLine($"The number {n} is Not Prime");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The number {n} is Neither Prime nor Composite");
                         }
                         break;
                     }
